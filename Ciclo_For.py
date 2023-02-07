841e392e0844910d264fd42b3989249c40bfd46c
Brihan200 <brayan3800@gmail.com>
2023-02-07 18:00:06 -0500

    Ciclo for

---
+++ Ciclo_For.py
@@ -0,0 +1,28 @@
+# Ciclo For
+
+"""
+* for -> Es una estructura de control que nos permite repetir un bloque de instrucciones(sentencias), cierta cantidad de veces
+
+         Sintaxis: for nombre_variable in objeto_iterable:            // Un objeto iterable es aquel que permite recorrer sus elementos uno a uno
+                       instruccion
+                       instruccion       
+"""
+
+#Ejemplo
+string = "Hola"
+
+for character in string:
+    print(character)
+
+print("\nFin del programa.")
+
+print()
+
+#Otro ejemplo
+print("\nOtro ejemplo for: \n")
+sttring = "Hola mundo"
+
+for characcter in sttring:
+    print(characcter)
+
+print("\nFin del programa.")
